44acdca1c153830741614e664ddfe48b3c42fe55
fabia <fabia@LAPTOP-6G1B96NL>
2022-04-21 22:16:42 -0600

    Se agrega un Jasper Report al proyecto, asi como sus respectivas clases y dependencias.

---
+++ src/main/java/com/tienda/demospringbootjasper/DemoSpringBootJasperApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.demospringbootjasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author fabia
+ */
+@SpringBootApplication
+public class DemoSpringBootJasperApplication {
+    
+    public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootJasperApplication.class, args);
+	}
+    
+}
